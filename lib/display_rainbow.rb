f5527343b969a55dd2656b96a27c71da6c6b6ace
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-07 03:42:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
+  puts colors
 end
+
+display_rainbow("R: red, O: orang
+e, Y: yellow, G: green, B: blue, I: indigo, V: violet\n")
